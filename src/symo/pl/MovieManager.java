f7ad1a422a6d9dd13ce99d60572359195522816e
luktka <lukasz.tkaczewski@gmail.com>
2016-07-28 17:46:51 +0200

    Hibernate v1.0 - Success

--- src/symo/pl/MovieManager.java
+++ src/symo/pl/MovieManager.java
@@ -10,25 +10,25 @@ public class MovieManager {
 	public static void main(String[] args) {
 		//creating configuration object  
 	    Configuration cfg = new Configuration();  
-	    //cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
+	    cfg.configure("symo/pl/hibernate.cfg.xml");//populates the data of the configuration file  
 	      
 	    //creating seession factory object  
-	    //SessionFactory factory=cfg.buildSessionFactory();  
+	    SessionFactory factory=cfg.buildSessionFactory();  
 	      
 	    //creating session object  
-	    //Session session=factory.openSession();  
+	    Session session=factory.openSession();  
 	      
 	    //creating transaction object  
-	    //Transaction t=session.beginTransaction();  
+	    Transaction t=session.beginTransaction();  
 	          
-	    //Movie m1=new Movie();  
-	    //m1.setId(115);  
-	    //m1.setName("sonoo");  
+	    Movie m1=new Movie();  
+	    m1.setId(115);  
+	    m1.setName("sonoo");  
 	      
-	    //session.persist(m1);//persisting the object  
+	    session.persist(m1);//persisting the object  
 	      
-	    //t.commit();//transaction is committed  
-	    //session.close();  
+	    t.commit();//transaction is committed  
+	    session.close();  
 	      
 	    System.out.println("successfully saved");  
 
